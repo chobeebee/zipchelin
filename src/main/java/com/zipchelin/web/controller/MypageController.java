8966f02a130076cd841804c88efa23eaf9b5826c
chobeebee <websera12@gmail.com>
2023-12-13 17:14:12 +0900

    명칭 카멜로 변경

--- src/main/java/com/zipchelin/web/controller/MypageController.java
+++ src/main/java/com/zipchelin/web/controller/MypageController.java
@@ -12,27 +12,27 @@ import lombok.RequiredArgsConstructor;
 public class MypageController {
 	
 	@GetMapping("/")
-    public String mypage() {
+    public String myPage() {
 		return "mypage/mypage";
 	}
 
 	@GetMapping("/myedit")
-	public String myedit() {
+	public String myEdit() {
 		return "mypage/myedit";
 	}
 
 	@GetMapping("/myheart")
-	public String myheart() {
+	public String myHeart() {
 		return "mypage/myheart";
 	}
 
 	@GetMapping("/mypost")
-	public String mypost() {
+	public String myPost() {
 		return "mypage/mypost";
 	}
 
 	@GetMapping("/myreply")
-	public String myreply() {
+	public String myReply() {
 		return "mypage/myreply";
 	}
 

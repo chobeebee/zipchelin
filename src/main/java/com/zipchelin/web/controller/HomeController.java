d974ec214b9d710cfeda1cbc471c6b3976e9ca8d
장민서 <philanthroo@naver.com>
2023-12-13 11:21:32 +0900

    관리자페이지 추가

--- src/main/java/com/zipchelin/web/controller/HomeController.java
+++ src/main/java/com/zipchelin/web/controller/HomeController.java
@@ -2,14 +2,16 @@ package com.zipchelin.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/*")
 public class HomeController {
 	
-	@GetMapping
+	@GetMapping("/index")
     public String Home() {
 		return "index";
 	}

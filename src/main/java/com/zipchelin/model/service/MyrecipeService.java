565b5ed93b4ccdd58821fb536c0ff921b80c4753
정해지 <jss_7801@naver.com>
2023-12-17 19:38:39 +0900

    service 오류 수정

--- src/main/java/com/zipchelin/model/service/MyrecipeService.java
+++ src/main/java/com/zipchelin/model/service/MyrecipeService.java
@@ -4,14 +4,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.zipchelin.model.dto.myrecipe.MyrecipeRequestDto;
-import com.zipchelin.repository.MyrecipeRepository;
-
-import lombok.RequiredArgsConstructor;
 
 @Service
-@RequiredArgsConstructor
 public class MyrecipeService {
-	private final MyrecipeRepository myrecipeRepository;
+	//private final MyrecipeRepository myrecipeRepository;
 	
 	@Transactional
 	public int selectMyrecipe(MyrecipeRequestDto params) {

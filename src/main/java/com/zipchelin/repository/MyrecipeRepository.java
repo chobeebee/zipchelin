3f81f46619837be87203901bc518c782568bbe4d
DESKTOP-59O051R\cho <websera12@gmail.com>
2023-12-17 19:27:27 +0900

    나만의 레시피 repository 병합

--- src/main/java/com/zipchelin/repository/MyrecipeRepository.java
+++ src/main/java/com/zipchelin/repository/MyrecipeRepository.java
@@ -2,17 +2,6 @@ package com.zipchelin.repository;
 
 import java.util.List;
 
-import com.zipchelin.domain.Myrecipe;
-
 public class MyrecipeRepository {
-
-	String addMyrecipe(Myrecipe Myrecipe);
 	
-	List<Myrecipe> selectAll();
-	
-	int selectMyrecipe(Myrecipe myrecipe);
-	
-	void delMyrecipe(int myreNum);
-	
-	void updateMyrecipe(Myrecipe myrecipe);
 }

5eeb5318afc12e76f3289b6a3776d760b7d60878
na1011 <zerus94@naver.com>
2023-12-19 21:01:55 +0900

    네이버 로그인, 이메일 인증 구현

--- src/main/java/com/zipchelin/web/controller/EditorController.java
+++ src/main/java/com/zipchelin/web/controller/EditorController.java
@@ -1,24 +1,17 @@
 package com.zipchelin.web.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.swing.text.View;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
-import com.zipchelin.model.service.MyrecipeService;
+import javax.servlet.http.HttpServletRequest;
 
-@Controller
+//@Controller
 @RequestMapping("smarteditor")
 public class EditorController {
 
-	@Autowired
-	MyrecipeService myrecipeService;
+//	@Autowired
+//	MyrecipeService myrecipeService;
 	
 	@RequestMapping("/")
 	public ModelAndView insertEditor(HttpServletRequest req, ModelMap model) throws Exception {

b3bb0ad7c36694979fbaff4529950c84097b84e7
na1011 <zerus94@naver.com>
2023-12-20 18:51:36 +0900

    이메일 인증 완료, 네이버 로그인 구현

--- src/main/java/com/zipchelin/model/service/MyrecipeServiceImpl.java
+++ src/main/java/com/zipchelin/model/service/MyrecipeServiceImpl.java
@@ -1,9 +1,6 @@
 package com.zipchelin.model.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.zipchelin.repository.MyrecipeRepository;
 
 @Service
 public class MyrecipeServiceImpl {

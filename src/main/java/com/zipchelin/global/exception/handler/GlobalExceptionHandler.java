6eface07b4c7d01af53e6fbd1bc6e9f4197a1c31
na1011 <zerus94@naver.com>
2023-12-21 11:08:33 +0900

    로그인 인증 처리 보안, 로그인 성공 시 이전 페이지로 리다이렉트, 실패 시 커스텀 에러메시지

--- src/main/java/com/zipchelin/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/zipchelin/global/exception/handler/GlobalExceptionHandler.java
@@ -1,8 +1,8 @@
 package com.zipchelin.global.exception.handler;
 
-import com.zipchelin.global.exception.BadRequestException;
-import com.zipchelin.global.exception.BusinessLogicException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,15 +11,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(BadRequestException.class)
-    String badRequestExceptionHandle(BadRequestException e, Model model) {
+    @ExceptionHandler(UsernameNotFoundException.class)
+    String userNameNotFoundHandle(UsernameNotFoundException e, Model model) {
+        log.info("가입되지 않은 아이디로 로그인 시도 = {}", e.getMessage());
         model.addAttribute("error", e.getMessage());
-        return "error/404";
+        return "/member/login";
     }
 
-    @ExceptionHandler(BusinessLogicException.class)
-    String businessLogicExceptionHandle(BusinessLogicException e, Model model) {
+    @ExceptionHandler(BadCredentialsException.class)
+    String badCredentialsHandle(BadCredentialsException e, Model model) {
+        log.info("잘못된 비밀번호로 로그인 시도 = {}", e.getMessage());
         model.addAttribute("error", e.getMessage());
-        return "error/500";
+        return "/member/login";
     }
 }

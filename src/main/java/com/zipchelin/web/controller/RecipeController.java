7bd8c96d70e5e8d7e2dfcefa15037254a971664a
ccw991155 <ccw991155@naver.com>
2023-12-22 09:19:59 +0900

    변경

--- src/main/java/com/zipchelin/web/controller/RecipeController.java
+++ src/main/java/com/zipchelin/web/controller/RecipeController.java
@@ -8,8 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.zipchelin.model.service.RecipeService;
-
 import lombok.RequiredArgsConstructor;
 
 @Controller
